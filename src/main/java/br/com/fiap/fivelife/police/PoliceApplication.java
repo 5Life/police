991dcbd300e566cc1a7f912b5dd88bffdbf270a5
Patricia Clares <patricia.clares@dasa.com.br>
2020-11-18 22:15:45 -0300

    first commit

---
+++ src/main/java/br/com/fiap/fivelife/police/PoliceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fivelife.police;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliceApplication.class, args);
+	}
+
+}
